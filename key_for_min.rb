01d9f544bc7fc777691de3e0d5f6be5d867cb304
github username <github email address>
2019-06-12 18:47:01 -0400

    Return key for min value in hash done

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,9 @@ def key_for_min_value(name_hash)
     name_hash.each do |current_key, current_value|
         if lowest_value == nil || current_value < lowest_value
         # Why does the if statement below throw an error? Is it because Ruby reads code from left to right, so its comparing current_value(an integer) to lowest_value (which starts off as nil)?
+        # Error thrown:
+        #  ArgumentError:
+        #  comparison of Integer with nil failed
         # if current_value < lowest_value || lowest_value == nil
             lowest_value = current_value
             lowest_key = current_key
